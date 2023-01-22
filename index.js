e83d446793a4071aaed357b46c3791158472f14f
Romanpuss <bryce_hrusovsky@yahoo.com>
2023-01-21 17:09:11 -0800

    Added Commands folder with ping, user, server com

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const { Client, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
-const client = new ClipboardEvent({intents: [GatewayIntentBits.Guilds] });
+const client = new Client({intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
 // We use 'c' for the event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c=> {
     console.log(`Ready! Logged in as ${c.user.tag}`);
 });
+
+// Log in to Discord with your client's token
+client.login(token);
